df638a2882b6a997d535cf45863725997505d59a
asahu786 <sahu.ashu@gmail.com>
2025-12-05 10:09:02 +0530

    Updated Jenkinsfile and project files

--- tests/src/test/java/com/galaxe/sampleci/ui/HelloPageTest.java
+++ tests/src/test/java/com/galaxe/sampleci/ui/HelloPageTest.java
@@ -1,43 +1,40 @@
 package com.galaxe.sampleci.ui;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.Test;
 import org.testng.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class HelloPageTest {
-
     private WebDriver driver;
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
-        // Configure Chrome options
-    	ChromeOptions options = new ChromeOptions();
-    	options.addArguments("--headless=new");
-    	options.addArguments("--no-sandbox");
-    	options.addArguments("--disable-dev-shm-usage");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-        // Connect to Selenium Grid (hub must be running in Docker Compose)
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-
+        driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
     }
 
     @Test
     public void testOpenHomePage() {
-    	driver.get("http://springboot-app:8081/");
-        String title = driver.getTitle();
-        System.out.println("Page title is: " + title);
-        String bodyText = driver.findElement(By.tagName("body")).getText();
+        driver.get("http://springboot-app:8081/");
+        WebElement body = driver.findElement(By.tagName("body"));
+        String bodyText = body.getText();
+        System.out.println("Page body text: " + bodyText);
         Assert.assertTrue(bodyText.contains("Hello World"), "Page should contain Hello World");
     }
 
