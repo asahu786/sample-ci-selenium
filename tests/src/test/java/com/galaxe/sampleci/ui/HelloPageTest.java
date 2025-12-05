28942349caa2ba96e5bfd9d07fb88c277d82b36a
asahu786 <sahu.ashu@gmail.com>
2025-12-05 08:34:50 +0530

    Updated Jenkinsfile and project files

--- tests/src/test/java/com/galaxe/sampleci/ui/HelloPageTest.java
+++ tests/src/test/java/com/galaxe/sampleci/ui/HelloPageTest.java
@@ -1,34 +1,43 @@
-// tests/src/test/java/com/galaxe/sampleci/ui/HelloPageTest.java
 package com.galaxe.sampleci.ui;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
-import static org.testng.Assert.assertTrue;
-
 public class HelloPageTest {
-  private WebDriver driver;
 
-  @BeforeClass
-  public void setUp() throws Exception {
-    ChromeOptions options = new ChromeOptions();
-    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-  }
+    private WebDriver driver;
 
-  @Test
-  public void helloEndpointLoads() {
-    driver.get("http://localhost:8081/hello");
-    assertTrue(driver.getPageSource().contains("Hello from Spring Boot App!"));
-  }
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Configure Chrome options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // run headless in CI
 
-  @AfterClass
-  public void tearDown() {
-    if (driver != null) driver.quit();
-  }
+        // Connect to Selenium Grid (hub must be running in Docker Compose)
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+    }
+
+    @Test
+    public void testOpenHomePage() {
+        driver.get("http://localhost:8081"); // your Spring Boot app URL
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+
+        // Simple assertion
+        assert title != null && !title.isEmpty() : "Page title should not be empty";
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
